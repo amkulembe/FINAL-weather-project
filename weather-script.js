23c19034441c34f1e5b19238630db3df5a70099a
amkulembe <amkulembe@gmail.com>
2025-02-19 20:44:36 +0200

    Updated the search engine

---
+++ weather-script.js
@@ -0,0 +1,9 @@
+function handleSearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = searchInput.value;
+}
+
+let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", handleSearchSubmit);
