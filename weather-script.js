5e4ecde94141539a6b03f94f6b342b565f5eccb7
amkulembe <amkulembe@gmail.com>
2025-02-20 14:19:08 +0200

    created a functioning search engine

--- weather-script.js
+++ weather-script.js
@@ -1,8 +1,22 @@
+function weatherRefresh(response) {
+  let temperatureElement = document.querySelector("#current-temperature");
+  let temperature = response.data.temperature.current;
+  let currentCityElement = document.querySelector("#current-city");
+  currentCityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "5f7d2db5081aa010a869f3ato494221e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(weatherRefresh);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-input");
-  let currentCityElement = document.querySelector("#current-city");
-  currentCityElement.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
